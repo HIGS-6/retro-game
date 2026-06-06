0134cf55311e0e3e6dac183b1f036a94fff98cc9
HIGS <higs.dev@gmail.com>
2026-06-06 22:56:06 +0100

    Changed Raylib Binding & Added Custom Features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use sola_raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = sola_raylib::init()
+        .size(640, 480)
+        .title("Retro Game")
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.draw_text("Hello, world!", 10, 10, 28, Color::BLACK);
+        d.clear_background(Color::WHEAT);
+    }
 }
